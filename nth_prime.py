08b2f879975cc701ad60c7da4f5f3896082d9c04
apierce1992 <apierce1992@gmail.com>
2015-12-01 14:58:49 -0500

    added comments for todos

--- nth_prime.py
+++ nth_prime.py
@@ -1,14 +1,17 @@
 
+#Current problem to fix identified via testing: Gets caught in endless loop when number
+#entered is below 3, does not raise errors for negative numbers.
+
 def is_prime(x):
     if x == 2:
         return True
     else:
         for number in range (3,x):
             if x % number == 0 or x % 2 == 0:
-         #print number
                 return False
-
+        print("True")
         return True
+
 
 def prime(n):
     primes = [2]
@@ -16,5 +19,7 @@ def prime(n):
         if is_prime(i) == True:
             primes.append(i)
         elif len(primes) == n:
+            print("The {}th prime is {}".format(n, primes[-1]))
             return primes[-1]
-    #print("The {}th prime is {}".format(n, primes[-1]))
+
+is_prime(2)
